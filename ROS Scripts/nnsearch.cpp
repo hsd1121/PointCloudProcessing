e4c996e4f6684f1fcc68e6d2c1815e0db1f847ca
hsd1121 <hsd1121@vt.edu>
2020-04-15 11:18:58 -0400

    cleaned nnsearch

--- ROS Scripts/nnsearch.cpp
+++ ROS Scripts/nnsearch.cpp
@@ -1,5 +1,8 @@
 #include <pcl/point_cloud.h>
 #include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/io/pcd_io.h>
 
 #include <iostream>
 #include <vector>
@@ -10,18 +13,22 @@ main (int argc, char** argv)
 {
   srand (time (NULL));
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-
-  // Generate pointcloud data
-  cloud->width = 1000;
-  cloud->height = 1;
-  cloud->points.resize (cloud->width * cloud->height);
-
-  for (std::size_t i = 0; i < cloud->points.size (); ++i)
+  if(argc != 2)
   {
-    cloud->points[i].x = 1024.0f * rand () / (RAND_MAX + 1.0f);
-    cloud->points[i].y = 1024.0f * rand () / (RAND_MAX + 1.0f);
-    cloud->points[i].z = 1024.0f * rand () / (RAND_MAX + 1.0f);
+      std::cerr << "Expecting input argument: file" << std::endl;
+      return -1;
+  }
+
+  std::ostringstream oss;
+  oss << argv[1];
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  if(pcl::io::loadPCDFile<pcl::PointXYZ> (oss.str(), *cloud) == -1)
+  {
+      PCL_ERROR("Couldn't read file ");
+      PCL_ERROR(oss.str().c_str());
+      PCL_ERROR("\n");
+      return (-1);
   }
 
   pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
@@ -41,18 +48,12 @@ main (int argc, char** argv)
   std::vector<int> pointIdxNKNSearch(K);
   std::vector<float> pointNKNSquaredDistance(K);
 
-  std::cout << "K nearest neighbor search at (" << searchPoint.x 
-            << " " << searchPoint.y 
-            << " " << searchPoint.z
-            << ") with K=" << K << std::endl;
-
   if ( kdtree.nearestKSearch (searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
   {
-    for (std::size_t i = 0; i < pointIdxNKNSearch.size (); ++i)
-      std::cout << "    "  <<   cloud->points[ pointIdxNKNSearch[i] ].x 
-                << " " << cloud->points[ pointIdxNKNSearch[i] ].y 
-                << " " << cloud->points[ pointIdxNKNSearch[i] ].z 
-                << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
+      std::cout << "    "  <<   cloud->points[ pointIdxNKNSearch[0] ].x 
+                << " " << cloud->points[ pointIdxNKNSearch[0] ].y 
+                << " " << cloud->points[ pointIdxNKNSearch[0] ].z 
+                << " (squared distance: " << pointNKNSquaredDistance[0] << ")" << std::endl;
   }
 
 
