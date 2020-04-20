a8ff993c04cae4fb4fd85f901812d64d8f6b4839
hsd1121 <hsd1121@vt.edu>
2020-04-20 16:31:35 -0400

    added octomap callback

---
+++ point_cloud_processing/src/bt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <octomap_msgs/Octomap.h>
+
+void octomap_cb(const octomap_msgs::Octomap& input)
+{
+      
+    count++;
+    std::cout << count << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+
+    ros::init(argc, argv, "point_cloud_processing");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("octomap_binary", 1, octomap_cb);
+
+    ros::spin();
+}
